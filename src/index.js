5b39d3e5a53e347ee0f8465fe6e5a44395bedeae
Scott Vinkle <svinkle@gmail.com>
2017-10-18 23:06:26 -0400

    Fixing build path.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App/>
-  </HashRouter>, document.getElementById('root'));
+  </BrowserRouter>, document.getElementById('root'));
 
 registerServiceWorker();
